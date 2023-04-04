a1311dd0001e362bb705ce911aa636a11a3e6875
Joel <ndohjoelmbj16@gmail.com>
2023-04-04 11:55:03 +0100

    Chnaged it to postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,29 @@
 const Sequelize = require('sequelize');
 const config = require('config');
+const logger = require('../shared/logger');
 
 const dbConfig = config.get('database');
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    dialect: dbConfig.dialect,
-    storage: dbConfig.storage,
-    logging: dbConfig.logging,
-  }
-);
+let sequelize;
+
+console.log(dbConfig.uri)
+
+if (!dbConfig.uri) {
+  console.log('I do not have the uri '+ dbConfig.uri)
+  logger.info(dbConfig.uri);
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+      dialect: dbConfig.dialect,
+      storage: dbConfig.storage,
+      logging: dbConfig.logging,
+    }
+    );
+  } else {
+  console.log('I have the uri '+ dbConfig.uri)
+  sequelize = new Sequelize(dbConfig.uri);
+}
 
 module.exports = sequelize;
